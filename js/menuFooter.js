aaf0183b94ad616c99357d5cad0dca6bf7590591
Mike Trejo <miguel.adrian.trejo@gmail.com>
2024-03-20 18:51:11 -0600

    js logic to arrow and dropuo down

--- js/menuFooter.js
+++ js/menuFooter.js
@@ -2,10 +2,14 @@ function windowSizeCheck() {
 
     $('#c2,#c3').off('click'); 
 
-    var degrees = 0;
-
     if($(window).width() < '768') {
 
+
+    var degreesStr = $("#arrMob > img").attr("data-degrees");
+
+    var degrees = 0;
+
+    console.log("grados init"+degreesStr);
 
         $(".cortina").toggle('fast',"swing",function(){
             $(this).attr("data-status","no-vis");
@@ -13,16 +17,21 @@ function windowSizeCheck() {
         })
 
 
-
         $('#c2,#c3').on('click', function(e) {
 
-            
+            degreesStr = $(this).find("#arrMob > img").attr("data-degrees");
 
-            if(degrees == 0){
-                degrees += -90;
+            console.log("grados click"+degreesStr);
+
+            if(degreesStr == "0" || degreesStr == undefined || degreesStr == null || degreesStr == 0){
+                degrees = -90;
+                console.log("true");
             }else{
-                degrees -= -90;
+                degrees = 0;
+                console.log("false");
             }
+
+            console.log("grados click num"+degrees);
 
 
             $(this).find("#arrMob > img").css({
@@ -31,7 +40,14 @@ function windowSizeCheck() {
                 '-moz-transform': 'rotate(' + degrees + 'deg)',
                 '-webkit-transform': 'rotate(' + degrees + 'deg)',
                 '-o-transform': 'rotate(' + degrees + 'deg)'
-              }); 
+              });
+              
+              
+            $(this).find("#arrMob > img").attr("data-degrees",degrees);
+
+            degreesStr = $(this).find("#arrMob > img").attr("data-degrees");
+
+            console.log("grados after click"+degreesStr);
 
 
             $(this).find(".cortina").stop().fadeTo('fast', 0, function() {
